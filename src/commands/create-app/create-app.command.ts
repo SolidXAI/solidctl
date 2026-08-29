1ef6747972992c7b3d835a0c1b626326ae49a4d2
Chetan <chetan.koli@logicloop.io>
2026-08-29 17:43:25 +0530

    refactor: remove syncCoreModuleIcon function and related references; add postinstall script for core module icon

--- src/commands/create-app/create-app.command.ts
+++ src/commands/create-app/create-app.command.ts
@@ -30,7 +30,6 @@ import {
   SOURCE_TEMPLATE_FOLDER_UI,
   TARGET_FOLDER_API,
   TARGET_FOLDER_UI,
-  syncCoreModuleIcon,
   updatePackageName,
   updatePortInPackageJson,
   updateSolidxPackageVersions,
@@ -226,7 +225,6 @@ export function registerCreateAppCommand(program: Command) {
           showLogs,
           stepOutput('Step 1: Setting up boilerplate for the backend...'),
         );
-        syncCoreModuleIcon(targetPath);
         spinner.succeed('Step 1: Backend boilerplate ready');
 
         // Step 2: Copy and install frontend
